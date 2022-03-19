76b946f80bfd3787c2464d890355015f917946f4
Smoke52ru <Smoke52ru@ya.ru>
2022-03-19 22:35:05 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Card, {CardVariant} from "./components/Card";
+import {ITodo, IUser} from "./types/types";
+import axios from 'axios';
+import List from './components/List';
+import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [users, setUsers] = useState<IUser[]>([])
+    const [todos,setTodos] = useState<ITodo[]>([])
+
+    useEffect(() => {
+        fetchUsers()
+        fetchTodos()
+    }, [])
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    async function fetchTodos() {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            setTodos(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
+    return (
+        <div>
+            <Card
+                height={'200px'}
+                width={'200px'}
+                variant={CardVariant.outlined}
+                onClick={(num) => console.log('click', num)}
+            >
+                <button>Button</button>
+            </Card>
+            <List
+                items={users}
+                renderItem={(user:IUser)=> <UserItem user={user} key={user.id}/>}
+            />
+            <List
+                items={todos}
+                renderItem={(todo:ITodo)=> <TodoItem todo={todo} key={todo.id}/>}
+            />
+        </div>
+    );
+};
 
 export default App;
